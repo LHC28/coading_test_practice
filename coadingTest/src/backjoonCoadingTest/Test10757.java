3c8cb95fbfa5ce819787bbe5eb291e562a056938
LHC28 <zzangth95@naver.com>
2022-08-03 23:46:14 +0900

    22.08.03 백준 10757문제 완료

---
+++ coadingTest/src/backjoonCoadingTest/Test10757.java
@@ -0,0 +1,18 @@
+package backjoonCoadingTest;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Test10757 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		BigInteger num1 = new BigInteger(scan.next()); 
+		BigInteger num2 = new BigInteger(scan.next());
+		
+		System.out.println(num1.add(num2));
+		
+		
+	}
+}
