08edfd3a3c4d6d7a671ce963cf4566bd24754fa7
LHC28 <zzangth95@naver.com>
2022-08-10 23:36:40 +0900

    22.08.10 백준 1929번 문제 완료

--- coadingTest/src/backjoonCoadingTest/Test1929.java
+++ coadingTest/src/backjoonCoadingTest/Test1929.java
@@ -7,26 +7,38 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Test1929 {
-
+	
+	public static void get_prime(int number) {
+		
+		if(number<2) {
+			return;
+		}else if(number==2) {
+			System.out.println(number);
+			return;
+		}
+		
+		for(int i=2; i<=Math.sqrt(number); i++) {
+			if(number%i==0) {
+				return;
+			}
+		}
+		System.out.println(number);
+		return;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String[] nums = br.readLine().split(" ");
+		int min = Integer.valueOf(nums[0]);
+		int max = Integer.valueOf(nums[1]);
 		
-		for(int i=Integer.parseInt(nums[0]); i<=Integer.parseInt(nums[1]); i++) {
-			int count = 0;
-			for(int j=1; j<=i; j++) {
-				if(i%j==0) {
-					count+=1;
-				}
-			}
-			if(count==2) {
-				bw.write(String.valueOf(i)+"\n");
-			}
+		for(int i=min; i<=max; i++) {
+			get_prime(i);
 		}
-		bw.flush();
-		bw.close();
+		
+		
 	}
 }
