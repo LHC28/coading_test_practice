dc3fc5309108af09ca38f11397857fe6ded46e6d
LHC28 <zzangth95@naver.com>
2022-08-19 21:10:34 +0900

    백준 9020 문제 중간

---
+++ coadingTest/src/backjoonCoadingTest/Test9020.java
@@ -0,0 +1,14 @@
+package backjoonCoadingTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test9020 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 어떻게 접근해야할지 고민이 좀 필요할듯...
+		
+	}
+}
